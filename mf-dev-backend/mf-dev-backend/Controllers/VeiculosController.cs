11598e058e707116764c0cdfe2f57c23a7fdea67
Lucas Santos <devlucas44@gmail.com>
2023-10-12 13:46:40 -0300

    first commit

---
+++ mf-dev-backend/mf-dev-backend/Controllers/VeiculosController.cs
@@ -0,0 +1,26 @@
+﻿using mf_dev_backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mf_dev_backend.Controllers
+{
+    public class VeiculosController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public VeiculosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var dados = await _context.Veiculos.ToListAsync();
+
+            return View(dados);
+        }
+    }
+}
